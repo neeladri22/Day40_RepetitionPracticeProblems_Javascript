75563468397415af7041b59635fc37f3dea8a491
neeladri22 <neeladri22@gmail.com>
2022-07-31 09:11:37 +0530

    Added the Prime Number in Range

--- RepetitionPracticeProblems.js
+++ RepetitionPracticeProblems.js
@@ -1,16 +1,16 @@
 
-var num =prompt("Please enter a number to check for prime number: ");
+var frange =prompt("Please enter first range");
+var lrange=prompt("Please enter last range")
 
-var count=0;
-for (var i=1;i<=num;i++){
-    if (num%i==0){
-        count=count+1;
+console.log("All prime number with in that range is:");
+for (i=frange;i<=lrange;i++){
+    var count=0;
+    for (var j=1;j<=i;j++){
+        if(i%j==0){
+            count++;
+        }
     }
-}
-
-if (count==2){
-    console.log("Entered number is Prime Number");
-}
-else{
-    console.log("Enter number is not a prime number")
+    if(count==2){
+        console.log(i);
+    }
 }
