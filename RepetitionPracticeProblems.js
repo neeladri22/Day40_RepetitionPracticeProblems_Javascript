ff013e51797983bc0a3e02d3c4ebfebcae8d50bf
neeladri22 <neeladri22@gmail.com>
2022-07-31 09:52:12 +0530

    Added program to flipcoin

--- RepetitionPracticeProblems.js
+++ RepetitionPracticeProblems.js
@@ -1,18 +1,15 @@
-function Tofahrenheit (){
-    var degC = prompt("Please enter Temperature in Celcius");
-    let degF = (degC * 9/5) +32;
-    console.log(degF);
-    
-}
+let HeadCount = 0;
+    let TailCount = 0;
 
-Tofahrenheit()
+    while (HeadCount < 11 | TailCount < 11) {
+        let toss = Math.floor(Math.random() * 10) % 2;
+        if (toss == 1) {
+            HeadCount++;
+        }
+        else {
+            TailCount++;
+        }
 
-
-function ToCelcius (){
-    var degF = prompt("Please enter Temperature in Fahrenheit");
-    let degC = (degF - 32) * 5/9;
-    console.log(degC);
-    
-}
-
-ToCelcius();
+    }
+    console.log("Head Count :" + HeadCount);
+    console.log("Tail Count :" + TailCount);
