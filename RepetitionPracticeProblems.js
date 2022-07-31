3b689fed563c608e84a3cb2f39fc0408dcb1ff07
neeladri22 <neeladri22@gmail.com>
2022-07-31 09:06:18 +0530

    Added the Prime Number

--- RepetitionPracticeProblems.js
+++ RepetitionPracticeProblems.js
@@ -1,1 +1,16 @@
-console.log("Welcome to Repetition programs");
+
+var num =prompt("Please enter a number to check for prime number: ");
+
+var count=0;
+for (var i=1;i<=num;i++){
+    if (num%i==0){
+        count=count+1;
+    }
+}
+
+if (count==2){
+    console.log("Entered number is Prime Number");
+}
+else{
+    console.log("Enter number is not a prime number")
+}
